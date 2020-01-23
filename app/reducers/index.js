79b8d92772fd1a0fcdcf46aa3ce2be248d57617e
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-01-23 10:45:35 -0700

    added reducer and initialState to ./reducers/index.js. Reducer has actions for register and login.

---
+++ app/reducers/index.js
@@ -0,0 +1,51 @@
+
+
+export const reducer = (state = initialState, action) => {
+    switch(action.type){
+        case REGISTER_USER_LOADING:
+            return {
+                ...state,
+                isFetching: true,
+                error: null
+            };
+        case REGISTER_USER_FAILED:
+            return {
+                ...state,
+                isFetching: false,
+                error: action.payload
+            }
+        case REGISTER_USER_SUCCESS:
+            return {
+                ...state,
+                isFetching: false,
+                error: null
+            }
+        case LOGIN_USER_LOADING:
+            return {
+                ...state,
+                isFetching: true,
+                error: null
+            };
+        case LOGIN_USER_FAILED:
+            return {
+                ...state,
+                isFetching: false,
+                error: action.payload
+            }
+        case LOGIN_USER_SUCCESS:
+            return {
+                ...state,
+                user: action.payload,
+                isFetching: false,
+                error: null
+            }
+        default:
+            return state;
+    }
+}
+
+const initialState = {
+    user:{},
+    error:null,
+    isFetching:false
+};

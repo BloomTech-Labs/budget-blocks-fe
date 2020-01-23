3441d053ecc1d114c311866486f99183c921595f
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-01-23 10:52:14 -0700

    added actions for logging in user. does not inclide axios calls for login

--- app/actions/index.js
+++ app/actions/index.js
@@ -13,3 +13,17 @@ export const registerUserFailure = error => ({
     type: REGISTER_USER_FAILED,
     payload: error
 });
+
+export const LOGIN_USER_LOADING = "LOGIN_USER_LOADING";
+export const LOGIN_USER_SUCCESS = "LOGIN_USER_SUCCESS";
+export const LOGIN_USER_FAILED = "LOGIN_USER_FAILED";
+
+export const loginUserLoading = () => ({ type: LOGIN_USER_LOADING });
+export const loginUserSuccess = data => ({
+    type: LOGIN_USER_SUCCESS,
+    payload: data
+  });
+export const loginUserFailure = error => ({
+    type: LOGIN_USER_FAILED,
+    payload: error
+});

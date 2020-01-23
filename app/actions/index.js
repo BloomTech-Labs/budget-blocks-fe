6bedb6121341ed1ed2cde029fd5deb84df297621
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-01-23 10:50:29 -0700

    added actions for register. does not include axios calls for register

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,1 +1,15 @@
 import axios from "axios";
+
+export const REGISTER_USER_LOADING = "REGISTER_USER_LOADING";
+export const REGISTER_USER_SUCCESS = "REGISTER_USER_SUCCESS";
+export const REGISTER_USER_FAILED = "REGISTER_USER_FAILED";
+
+export const registerUserLoading = () => ({ type: REGISTER_USER_LOADING });
+export const registerUserSuccess = data => ({
+    type: REGISTER_USER_SUCCESS,
+    payload: data
+  });
+export const registerUserFailure = error => ({
+    type: REGISTER_USER_FAILED,
+    payload: error
+});

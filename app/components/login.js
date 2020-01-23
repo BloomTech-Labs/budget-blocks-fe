16d0d4c930a4e17672fb155294499df2d039656c
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-01-23 16:10:06 -0700

    imported useState to Login.js. created username and password variables with useState an created inputs for username and password on the form. added a button for submitting form

--- app/components/login.js
+++ app/components/login.js
@@ -1,13 +1,28 @@
-import React from "react"
+import React, { useState } from "react"
 import { connect } from "react-redux"
 
 const Login = (props)=>{
+    const [username,setUsername] = useState('');
+    const [password,setPassword] = useState('');
+
     return(
 
-    <div className="login">
+    <div className="login" onSubmit={handleSubmit}>
         <h2>Login</h2>
         <form className="LoginForm">
-            
+        <input type= "text" 
+            name="username" 
+            onChange={handleChange} 
+            value={username} 
+            placeholder="Enter Username"
+        />
+        <input type= "text" 
+            name="password" 
+            onChange={handleChange} 
+            value={password} 
+            placeholder="Enter Password"
+        />
+        <button>Login</button>
         </form>
     </div>
 

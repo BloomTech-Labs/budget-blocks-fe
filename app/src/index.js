a66a236b6628bc110d95889e42de8243d57c1721
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-01-23 10:37:52 -0700

    imported applyMiddleware,createStore from redux, thunk from redux-thunk, logger from redux-logger, Provider from react-redux, and reducer from ./reducers

--- app/src/index.js
+++ app/src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { applyMiddleware,createStore} from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { Provider } from "react-redux";
+import {reducer} from "./reducers"
 
 
 ReactDOM.render(<App />, document.getElementById('root'));

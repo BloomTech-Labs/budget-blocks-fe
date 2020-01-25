4b318d5d28fd121bf0e42f50d1f4f23602dd9aba
adeoye adela <adeoye.adela@gmail.com>
2020-01-25 16:42:16 +0100

    added dependencies

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from "./components/login.js"
 
 function App() {
   return (
@@ -17,7 +18,9 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+         
         </a>
+        <Login/>
       </header>
     </div>
   );

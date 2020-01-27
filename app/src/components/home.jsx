a5e2c8fd72b1df1f1a4d1c3684daeeba25bbe1c3
adeoye adela <adeoye.adela@gmail.com>
2020-01-27 21:38:04 +0100

    Register component working

--- app/src/components/home.jsx
+++ app/src/components/home.jsx
@@ -1,42 +1,35 @@
 import React from "react";
-// import { connect } from "react-redux"
-// import { loginUser } from "../redux/actions"
-import { makeStyles } from '@material-ui/core/styles';
-
 import logo from "../media/image/logo.jpg";
 import budgetImg from "../media/image/budget_blocks.png";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import Button from "@material-ui/core/Button";
+import {Link} from "react-router-dom"
 
-const useStyles = makeStyles(theme => ({
-  margin: {
-    margin: theme.spacing(1)
-  }
- 
-}));
+
 const Home = props => {
-    const classes = useStyles();
 
   return (
-    <>
-      <CssBaseline />
-      <Container maxWidth="sm">
-        <div style={{ backgroundColor: "#ffffff", height: "100vh" }}>
-          <img src={logo} className="App-logo" alt="logo" />
-          <img src={budgetImg} className="budget_img" alt="budget_blocks" />
-          <div className="buttons">
-            <Button variant="outlined" className="signup">
-              Sign Up
-            </Button>
-            <Button variant="outlined" className="signin">
-              Sign In
-            </Button>{" "}
-          </div>
+    <div className="home" >
+    <Container maxWidth="sm">
+      <div style={{ backgroundColor: "#ffffff", height: "100vh" }}>
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={budgetImg} className="budget_img" alt="budget_blocks" />
+        
+        <div className="buttons">
+          <Link to="/register" className="links">
+          <Button variant="outlined" className="signup">
+            Sign Up
+          </Button>
+          </Link>
+          <Button variant="outlined" className="signin">
+            Sign In
+          </Button>
+         
         </div>
-      </Container>
-    </>
+      </div>
+    </Container>
+    </div>
+
   );
 };
 

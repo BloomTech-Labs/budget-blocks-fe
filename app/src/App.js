be9f36e7121507dec3cba29619e6fa098abbcb3b
Vanna Sok <53488795+vannasok@users.noreply.github.com>
2020-01-29 13:23:10 -0800

    navbar component

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-
+import NavBar from '../src/components/navbar';
 import './App.css';
 
 function App() {
 	return (
 		<div className='App'>
-			<div className='navbar'>
-				<h1>Nav Bar Here</h1>
-			</div>
+			<NavBar />
 		</div>
 	);
 }
--- app/src/components/navbar.js
+++ app/src/components/navbar.js
@@ -1,1 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import logo from '../media/images/logo.jpg';
+import budget from '../media/images/budget_blocks.png';
+
+const useStyles = makeStyles({
+	root: {
+		flexGrow: 1
+	}
+});
+
+export default function CenteredTabs() {
+	const classes = useStyles();
+	const [value, setValue] = React.useState(0);
+
+	const handleChange = (event, newValue) => {
+		setValue(newValue);
+	};
+
+	return (
+		<div className='navbar-container'>
+			<div className='logo-img'>
+				{/* <img src={logo} className='logo-img' alt='logo' /> */}
+				<img src={budget} className='logo-label' alt='logo label' />
+			</div>
+			<Paper className={classes.root}>
+				<Tabs
+					value={value}
+					onChange={handleChange}
+					indicatorColor='primary'
+					textColor='primary'
+					centered
+				>
+					<Tab label='My Dashboard' />
+					<Tab label='My Profile' />
+					<Tab label='Sing Out' />
+				</Tabs>
+			</Paper>
+		</div>
+	);
+}

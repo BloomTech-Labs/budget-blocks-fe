8dde2adcc198618c0aeade5c9acf9421257d4b30
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-01-30 13:51:54 -0700

    removed all information that is already in place in the Plaid Link

--- app/src/components/LinkAccount.js
+++ app/src/components/LinkAccount.js
@@ -1,10 +1,5 @@
 import React from "react";
-import Typography from "@material-ui/core/Typography";
-import TextField from "@material-ui/core/TextField";
 import "../style/linkAccountStyle.css";
-import { Link } from "react-router-dom";
-import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
-import Button from "@material-ui/core/Button";
 import PlaidLink from 'react-plaid-link';
 import { connect } from "react-redux";
 import { sendLinkToken } from "../redux/actions/PlaidActions";
@@ -19,19 +14,7 @@ const LinkWarning = (props)=>{
       }
 
     return(
-        <div className="LinkWarning">
-            <div className="info">
-                <Typography variant="body1" className="WarningTitle">Budget Blocks uses Plaid to link your bank</Typography>
-                <div className="confirmed">
-                    <CheckCircleOutlineIcon/>
-                    <Typography variant="body1" className="WarningInfo">Secure Transfer of your information is encrypted end-to-end </Typography>
-                </div>
-                <div className="confirmed">
-                    <CheckCircleOutlineIcon/>
-                    <Typography variant="body1" className="WarningInfo">Private Your credentials will never be made accessible to Budget Blocks </Typography>
-                </div>
-            </div>
-            <div className="buttons">
+        <div className="LinkAccount">
                 <PlaidLink
                     clientName="Budget Blocks"
                     env="sandbox"
@@ -42,13 +25,6 @@ const LinkWarning = (props)=>{
                     className="plaidButton">
                     Connect Bank Account
                 </PlaidLink> 
-
-                <Link to="/link" className="links">
-                    <Button variant="outlined" className="signin">
-                    Add Manual Income
-                    </Button>
-                </Link>
-            </div>
 
         </div>
     )

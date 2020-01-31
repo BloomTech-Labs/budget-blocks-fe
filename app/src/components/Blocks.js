dba88df7d1b68819adeab5c8646d982387b8d4f3
Lambda_School_Loaner_182 <lambda_school_loaner_182@Lambda-School-Loaner-182s-MacBook-Pro.local>
2020-01-31 10:49:59 -0500

    "hello"

--- app/src/components/Blocks.js
+++ app/src/components/Blocks.js
@@ -5,13 +5,27 @@ import {Row,Col} from 'react-bootstrap'
 import { Progress,Container,Table } from "reactstrap";
 import "./table.css"
 import axios from "axios"
+import immutable from "immutable"
   function DashboardBlocks (){
     let [categories,setCategories] = useState([])
+    const [click,setClick] = useState(false)
+    const [filter,setFilter] = useState([])
+    const[results,setResults] = useState([])
+
     useEffect(() => {
       axios.get("https://lambda-budget-blocks.herokuapp.com/api/users/categories/1")
-      .then(i => setCategories(i.data))
+      .then(i => {
+          setCategories(i.data)
+         setFilter(i.data.slice(Math.max(i.data.length - 5, 1)))
+      })
     },[])
-    categories = categories.filter(i => i.id < 6)
+    
+    console.log(filter)
+    const handleClick = () => {
+        setClick(!click)
+    }
+    console.log(categories)
+    
     console.log(categories)
   return (
     <Table responsive>
@@ -27,7 +41,8 @@ import axios from "axios"
       </tr>
     </thead>
     <tbody>
-    {categories.map(i => (
+        {click ? 
+    filter.map(i => (
       <tr key={i.id}>
         <td></td>
         <td>{i.category}</td>
@@ -39,59 +54,23 @@ import axios from "axios"
         <td>$500.00</td>
         <td>Edit</td>
       </tr>
+    )) :
+    categories.map(i => (
+        <tr key={i.id}>
+          <td></td>
+          <td>{i.category}</td>
+          <td>
+              <Progress value={i.id} />
+         </td>
+          <td>&#9989;</td>
+          <td>$0.00</td>
+          <td>$500.00</td>
+          <td>Edit</td>
+        </tr>
     ))}
     </tbody>
+    <button onClick={handleClick}>{click ? "View All" :"View Less"}</button>
   </Table>
-
   )
         }
 export default DashboardBlocks
-//   <Table responsive>
-//   <thead>
-//     <tr>
-//       <th></th>
-//       <th>Block</th>
-//       <th>Current status</th>
-//       <th></th>
-//       <th>Total Expenses</th>
-//       <th>Limit</th>
-//       <th>Action</th>
-//     </tr>
-//   </thead>
-//   <tbody>
-//     <tr>
-//       <td></td>
-//       <td>Title</td>
-//       <td>
-//           <Progress value={90} />
-//      </td>
-//       <td>&#9989;</td>
-//       <td>$0.00</td>
-//       <td>$500.00</td>
-//       <td>Edit</td>
-//     </tr>
-//     <tr>
-//       <td></td>
-//       <td>Title</td>
-//       <td>
-//           <Progress value={90} /></td>
-//       <td>&#9989;</td>
-//       <td>$0.00</td>
-//       <td>$500.00</td>
-//       <td>Edit</td>
-//     </tr>
-//     <tr>
-//       <td></td>
-//       <td>Title</td>
-//       <td>
-//           <Progress value={90} /></td>
-//       <td>&#9989;</td>
-//       <td>$0.00</td>
-//       <td>$500.00</td>
-//       <td>Edit</td>
-//     </tr>
-//   </tbody>
-// </Table>)
-    //   }
-
-  

40d1923404c84fba95a9d5688c2bacba69e674c6
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-02-03 16:25:57 -0700

    added test for register: Form errors when passwords do not match

--- app/src/components/tests/Register.spec.js
+++ app/src/components/tests/Register.spec.js
@@ -16,5 +16,31 @@ test('Register renders correctly',()=>{
     )).toMatchSnapshot();
 });
 
+test('Form errors when passwords do not match',()=>{
+    const callAPI = jest.fn();
+
+    const wrapper = mount(
+    <Router>
+        <Register registerUser={callAPI} />
+    </Router>
+    );
+
+
+    let passInput = wrapper.find("input").at(1);
+    let emailInput = wrapper.find("input").at(0);
+    let confirmPassInput = wrapper.find("input").at(2);
+
+    passInput.simulate('change',{ target: { value: 'true',name:"password" } });
+    emailInput.simulate('change',{ target: { value: 'sendtests@gmail.com',name:"email" } });
+    confirmPassInput.simulate('change',{ target: { value: 'false',name:"confirmPassword" } });
+
+    wrapper.find("form").simulate("submit");
+    
+    let errorText = wrapper.find("p").get(2);
+    
+    expect(errorText.props.children).toBe("Password Mismatch");
+    expect(callAPI).not.toHaveBeenCalled();
+});
+
 
 

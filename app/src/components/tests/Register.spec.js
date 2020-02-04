bbdf64f9562381388cbc3b33b94e5834130f0113
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-02-03 17:57:01 -0700

    added tests in register:When fields are empty and press sign up, sign up button disables, Form calls api when form is filled out correctly

--- app/src/components/tests/Register.spec.js
+++ app/src/components/tests/Register.spec.js
@@ -42,5 +42,43 @@ test('Form errors when passwords do not match',()=>{
     expect(callAPI).not.toHaveBeenCalled();
 });
 
+test('When fields are empty and press sign in, sign up button disables',()=>{
+    const callAPI = jest.fn();
+    const { getAllByText, getByText } = render(
+        <Router>
+            <Register registerUser={callAPI} />
+        </Router>
+    );
+    const signInButton = getAllByText(/Sign Up/i)[1];
+    fireEvent.click(signInButton.parentElement);
+    expect(signInButton.parentElement.hasAttribute("disabled")).toBe(true);
+    const passHelpTxt = getAllByText(/password is required/i)[0];
+    expect(passHelpTxt.innerHTML).toBe("password is required");
+    const emailHelpTxt = getByText(/email is required/i);
+    expect(emailHelpTxt.innerHTML).toBe("email is required");
+    expect(callAPI).not.toHaveBeenCalled();
+    
+});
+
+test('Form calls api when form is filled out correctly',()=>{
+    const callAPI = jest.fn();
+
+    const wrapper = mount(
+    <Router>
+        <Register registerUser={callAPI} />
+    </Router>
+    );
 
 
+    let passInput = wrapper.find("input").at(1);
+    let emailInput = wrapper.find("input").at(0);
+    let confirmPassInput = wrapper.find("input").at(2);
+
+    passInput.simulate('change',{ target: { value: 'true',name:"password" } });
+    emailInput.simulate('change',{ target: { value: 'sendtests@gmail.com',name:"email" } });
+    confirmPassInput.simulate('change',{ target: { value: 'true',name:"confirmPassword" } });
+
+    wrapper.find("form").simulate("submit");
+    
+    expect(callAPI).toHaveBeenCalled();
+});

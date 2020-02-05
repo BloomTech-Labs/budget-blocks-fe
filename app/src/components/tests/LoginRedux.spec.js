95f87e01a6e2cce729757d07292de1e8e5cdac7a
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-02-04 17:18:01 -0700

    added axios mock library as dev dependency. added test for successful login

--- app/src/components/tests/LoginRedux.spec.js
+++ app/src/components/tests/LoginRedux.spec.js
@@ -1,4 +1,18 @@
 import * as actions  from "../../redux/actions/LoginActions"
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import logger from "redux-logger";
+var MockAdapter = require('axios-mock-adapter');
+import axios from "axios"
+
+const middlewares = [thunk, logger];
+const mockStore = configureMockStore(middlewares);
+var mock = new MockAdapter(axios);
+const history = {
+    push: (string)=>{console.log(`Go to ${string}!`)}
+}
+
+
 
 test('should create an action to add a user on successful login',()=>{
     const payload = {
@@ -29,3 +43,26 @@ test('should create an action to add an error when login fails',()=>{
     };
     expect(actions.loginUserFailure(payload)).toEqual(expectedAction)
 });
+
+test('creates LOGIN_USER_SUCCESS when login is successful',()=>{
+    const returnBody = {
+        "id": 1,
+        "token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJlbWFpbCI6Ikt5bGUiLCJpYXQiOjE1ODAyMzU0NzMsImV4cCI6MTU4MDI0OTg3M30.vOU1ZbHcLxOgCtD50Pu7JqHiudEc-0VYtDfbtXeqvlU",
+        "message": "Welcome Yeet!",
+        "LinkedAccount": false
+    }
+
+    mock.onPost('https://lambda-budget-blocks.herokuapp.com/api/auth/login').reply(200, returnBody);
+    
+
+    const expectedActions = [
+      { type: actions.LOGIN_USER_LOADING },
+      { type: actions.LOGIN_USER_SUCCESS, payload: returnBody }
+    ]
+
+    const store = mockStore({ user:{} })
+    return store.dispatch(actions.loginUser({}, history)).then(() => {
+      // return of async actions
+      expect(store.getActions()).toEqual(expectedActions)
+    })
+})

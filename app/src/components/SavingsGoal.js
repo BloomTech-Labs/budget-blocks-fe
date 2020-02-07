689dc5ad63747432787f33e52cf27b030cbc8325
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-02-07 11:54:01 -0700

    removed commented out code and removed imports that were not being used.

--- app/src/components/SavingsGoal.js
+++ app/src/components/SavingsGoal.js
@@ -1,76 +1,12 @@
 import React,{useEffect,useState} from 'react';
 import axios from "axios"
-import Paper from '@material-ui/core/Paper';
-import Divider from '@material-ui/core/Divider';
 import Grid from '@material-ui/core/Grid';
 import styled from 'styled-components'
-
-// const useStyles = makeStyles(theme => ({
-//   container: {
-//     display: 'grid',
-//     gridTemplateColumns: 'repeat(12, 1fr)',
-//     gridGap: theme.spacing(3),
-//   },
-//   paper: {
-//     padding: theme.spacing(1),
-//     textAlign: 'center',
-//     color: theme.palette.text.secondary,
-//     whiteSpace: 'nowrap',
-//     marginBottom: theme.spacing(1),
-//   },
-//   divider: {
-//     margin: theme.spacing(2, 0),
-//   },
-// }));
-
-// export default function SavingsGoal() {
-//     const [categories,setCategories] = useState([])
-//     useEffect(() => {
-//         axios.get("https://lambda-budget-blocks.herokuapp.com/api/users/categories/1")
-//         .then(i => {
-//            setCategories(i.data.slice(Math.max(i.data.length - 3, 1)))
-//         })
-//       },[])
-//   const classes = useStyles();
-
-// console.log(categories)
-//   return (
-//     <div>
-//       <Typography variant="subtitle1" gutterBottom>
-//       </Typography>
-//       <Grid container spacing={3}>
-//         <Grid item xs={7}>
-//           <b>Savings Goal</b>
-//         </Grid>
-//         <Grid item xs={5}>
-//           <p></p>
-//           </Grid>
-//           {categories.length === 0 ? <p>Please make goals</p> :
-//           categories.map(i => (
-//               <>
-//          <Grid item xs={7}>
-//          <p>goal</p>
-//        </Grid>
-//        <Grid item xs={5}>
-//        <P>{i.id}</P>
-//          <p>{i.category}</p>
-//          </Grid>
-//          </>
-//           )
-//           )
-//         }
-//         <Button>he</Button>
-              
-//         </Grid>
-//     </div>
-//   );
-// }
 
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({

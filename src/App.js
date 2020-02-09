fc51fd8152132f5fd87588d36743fb16b3fba7b3
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-02-09 15:11:48 -0700

    removed linked account from routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-// import logo from "./logo.svg";
 import "./App.css";
 import Home from "./components/home";
 import Login from "./components/login";
 import Register from "./components/register";
 import Profile from "./components/CreateProfile"
 import { Switch, Route } from "react-router-dom";
-import LinkAccount from "./components/LinkAccount";
 import Dashboard from "./components/Dashboard";
 import BudgetGoal from "./components/PresetModal";
 
@@ -20,7 +18,6 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
-        <Route path="/link" component={LinkAccount} />
         <Route path="/profile" component={Profile} />
         <Route path="/dashboard" component={Dashboard} />
         {/* <Route path="/budgetModal" component={Blocks}/> */}

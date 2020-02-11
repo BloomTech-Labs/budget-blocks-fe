f21a2c4a5627f2d900b94602c9fe7dae52a611ba
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-02-11 15:34:02 -0700

    added more styling to balance component

---
+++ src/style/balanceStyle.js
@@ -0,0 +1,14 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles({
+	NoBalance: {
+		minWidth: 275,
+        background: 'lightgrey',
+        display: "flex",
+        padding:"2rem",
+        marginBottom:"2rem",
+        marginTop:"2rem",
+        alignItems:"center",
+        justifyContent:"space-between"
+	}
+});

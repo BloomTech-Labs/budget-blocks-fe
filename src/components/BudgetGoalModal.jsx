a179de410156034ae4f884a5cf37b8f3072066fa
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-02-11 00:20:26 -0700

    removed all unused functions and imports in BudgetGoalModal.js

--- src/components/BudgetGoalModal.jsx
+++ src/components/BudgetGoalModal.jsx
@@ -4,11 +4,7 @@ import { Modal_Title } from "./Modal_Components/Modal_Title";
 
 import { withStyles, makeStyles } from "@material-ui/core/styles";
 import Dialog from "@material-ui/core/Dialog";
-import MuiDialogTitle from "@material-ui/core/DialogTitle";
 import MuiDialogContent from "@material-ui/core/DialogContent";
-import MuiDialogActions from "@material-ui/core/DialogActions";
-import IconButton from "@material-ui/core/IconButton";
-import CloseIcon from "@material-ui/icons/Close";
 import Typography from "@material-ui/core/Typography";
 import Slider from "@material-ui/core/Slider";
 import TextField from "@material-ui/core/TextField";
@@ -37,36 +33,11 @@ const styles = theme => ({
   }
 });
 
-const DialogTitle = withStyles(styles)(props => {
-  const { children, classes, onClose, ...other } = props;
-  return (
-    <MuiDialogTitle disableTypography className={classes.root} {...other}>
-      <Typography variant="h6">{children}</Typography>
-      {onClose ? (
-        <IconButton
-          aria-label="close"
-          className={classes.closeButton}
-          onClick={onClose}
-        >
-          <CloseIcon />
-        </IconButton>
-      ) : null}
-    </MuiDialogTitle>
-  );
-});
-
 const DialogContent = withStyles(theme => ({
   root: {
     padding: theme.spacing(2)
   }
 }))(MuiDialogContent);
-
-const DialogActions = withStyles(theme => ({
-  root: {
-    margin: 0,
-    padding: theme.spacing(1)
-  }
-}))(MuiDialogActions);
 
 export default function BudgetGoal(props) {
   

1e223a9f0bd08d144289bbc371d927c5b9f3314d
Vanna Sok <53488795+vannasok@users.noreply.github.com>
2020-02-11 15:28:07 -0800

    new total budget display

--- src/components/UnlinkedTotalBudget.js
+++ src/components/UnlinkedTotalBudget.js
@@ -43,33 +43,21 @@ const TotalBudget = props => {
 			<CardContent>
 				<Typography variant='subtitle1' gutterBottom></Typography>
 				<Grid container spacing={3}>
-					<Grid item xs={12}>
-						Total Budget
-					</Grid>
-					<Grid item xs={12}>
-						<h3>
-							$ <span></span>
+					<Grid item xs={6}>
+						<h3>Total Expenses</h3>
+						<p>
+							$
 							{Math.round(
 								100 *
-									props.budget.reduce(function(a, b) {
+									props.expenses.reduce(function(a, b) {
 										return a + b;
 									}, 0)
 							) / 100}
-						</h3>
-					</Grid>
-
-					<Grid item xs={12}>
-						<Progress color='success' className='progress' value='0.00' />
+						</p>
 					</Grid>
 					<Grid item xs={6}>
-						<p>Income</p>
-						<p>+ $00.00</p>
-						<Icon className='icons green' icon={chevronCircleUp} />
-					</Grid>
-					<Grid item xs={6}>
-						<p>Expenses</p>
-						<p>- $0.00</p>
-						<Icon className='icons red' icon={chevronCircleDown} />
+						<h3>Goal</h3>
+						<p>total limit</p>
 					</Grid>
 				</Grid>
 			</CardContent>

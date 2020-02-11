07127744384f330c14fc577f8429b76dca562833
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-02-10 23:23:14 -0700

    removed hard coded data and put back the getTransactions function

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,6 +1,4 @@
 import React,{useEffect} from "react";
-import LinkAccount from "./LinkAccount";
-import plaidImg from "../media/image/PlaidIcon.png";
 import { connect } from "react-redux";
 import {getUserInfo} from '../redux/actions/ProfileActions'
 
@@ -18,23 +16,6 @@ export const Balance = ({balance})=>{
         </h1>
          
     )
-    // if(props.LinkedAccount ===true ){
-    //     return(
-    //         <div className="Balance">
-    //             <div className="myBalance">
-    //                 <p>My Balance</p>
-    //                 <h3>{`$${props.balance}`}</h3>
-    //             </div>
-    //         </div>
-    //     )
-    // }else{
-    //     return(
-    //         <div className="NoBalance">
-    //             <LinkAccount/>
-    //             <img src={plaidImg} className="plaidIcon" alt="Plaid Icon"/>
-    //         </div>
-    //     )
-    // }
 }
 
 function mapStateToProps(state){

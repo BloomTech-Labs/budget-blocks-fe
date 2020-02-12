7cee82c3213803847210b602e1db46d6b9f62143
Lambda_School_Loaner_182 <lambda_school_loaner_182@Lambda-School-Loaner-182s-MacBook-Pro.local>
2020-02-11 23:19:44 -0500

    "changed the transactons"

--- src/components/LinkedTransactions.js
+++ src/components/LinkedTransactions.js
@@ -19,7 +19,7 @@ const useStyles = makeStyles(theme => ({
 		width: '100%',
 		marginBottom: '1rem',
 		height: '70%',
-		background: 'lightgrey'
+		background: '#F0F0F0'
 	},
 	details: {
 		display: 'flex'

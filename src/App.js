81cecdb8e3b14e014dafc4f6d5b78f650fe08047
Vanna Sok <53488795+vannasok@users.noreply.github.com>
2020-02-14 12:35:13 -0800

    google analytics setup

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
-import React from "react";
-import "./App.css";
-import Home from "./components/home";
-import Login from "./components/login";
-import Register from "./components/register";
-import Profile from "./components/CreateProfile"
-import { Switch, Route } from "react-router-dom";
-import Dashboard from "./components/Dashboard";
-import PrivateRoute from "./components/PrivateRoute";
-import BudgetGoal from "./components/PresetModal";
-import Navbar from "./components/NavBar"
+import React from 'react';
+import './App.css';
+import Home from './components/home';
+import Login from './components/login';
+import Register from './components/register';
+import Profile from './components/CreateProfile';
+import { Switch, Route } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import PrivateRoute from './components/PrivateRoute';
+import BudgetGoal from './components/PresetModal';
+import Navbar from './components/NavBar';
+import ReactGA from 'react-ga';
 
+function initializeReactGA() {
+	ReactGA.initialize('UA-158611259-1');
+	ReactGA.pageview('/homepage');
+}
 
 function App() {
-  
-  return (
-    <div>
-    <Navbar /> 
-    <div className="App">
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <PrivateRoute path="/profile" component={Profile} />
-        <PrivateRoute path="/dashboard" component={Dashboard} />
-        <Route path="/budgetModal" component={BudgetGoal}/>
-
-     
-
-
-      </Switch>
-    </div>
-    </div>
-  );
+	return (
+		<div>
+			<Navbar />
+			<div className='App'>
+				<Switch>
+					<Route exact path='/' component={Home} />
+					<Route path='/login' component={Login} />
+					<Route path='/register' component={Register} />
+					<PrivateRoute path='/profile' component={Profile} />
+					<PrivateRoute path='/dashboard' component={Dashboard} />
+					<Route path='/budgetModal' component={BudgetGoal} />
+				</Switch>
+			</div>
+		</div>
+	);
 }
 
 export default App;

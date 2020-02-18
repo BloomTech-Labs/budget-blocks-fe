04e33b079e058a14534271bafa2267e1bf279019
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-02-18 13:38:45 -0700

    refactored tablehead to use regular html instaed of materialui. Successful login now stores data in session storage. initial state will pull from session storage if data is there

--- src/components/Blocks_Components/LinkedBlocks.js
+++ src/components/Blocks_Components/LinkedBlocks.js
@@ -31,7 +31,7 @@ export function Blocks(props) {
 		budget:0.00
 	})
 	const handleClickOpen = (id, budget) => {
-		setValues({...values,userId:localStorage.id, catId:id, budget});
+		setValues({...values,userId:props.userID, catId:id, budget});
 		setOpen(true);
 	};
 	const handleClose = () => {

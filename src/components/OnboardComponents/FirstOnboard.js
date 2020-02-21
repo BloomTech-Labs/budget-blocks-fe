be2fcd25d9c1f3dc423313d35333851139c16bb7
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-02-21 12:03:25 -0700

    register takes user to login. login checks to see if linkedaccount. if it isnt then it goes to onboarding. changed onboarding component so that manual is a button and not a link. created files for manual redux actions/reducer

--- src/components/OnboardComponents/FirstOnboard.js
+++ src/components/OnboardComponents/FirstOnboard.js
@@ -1,23 +1,23 @@
-import React,{useEffect,useState} from "react"
+import React from "react"
+import axios from "axios";
 import Balance from "../Balance_Components/Balance"
-import {loginUser} from "../../redux/actions/LoginActions"
 import "./onboard.css"
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import Loader from 'react-loader-spinner'
 
 const FirstOnboard = props => {
-    useEffect(() => {
-props.loginUser({email:localStorage.getItem("user_email"),password:localStorage.getItem("user_password")})
-    },[])
-    console.log(props.error)
+    const handleClick = (e) =>{
+        axios.get(`https://lambda-budget-blocks.herokuapp.com/manual/onboard/${props.userId}`)
+            .then((response)=>{props.history.push("/onBoard/select")});
+    }
     return (
         
        <div>
             {!props.isFetching && !props.linkedAccount && props.error === null
             ?
             <div className="main">
-        <div className="manualBudgetButton"> <Link to="/onboard/budget">Manually set your budget goals here</Link></div>
+        <div className="manualBudgetButton"> <button onClick={handleClick}>Manually set your budget goals here</button></div>
         <Balance /> 
         </div>
         :
@@ -37,7 +37,7 @@ props.loginUser({email:localStorage.getItem("user_email"),password:localStorage.
       :
       <div className="main">
       <p className="error">Sorry Please Try Again</p>
-  <div className="manualBudgetButton"> <Link to="/onboard/budget">Manually set your budget goals here</Link></div>
+  <div className="manualBudgetButton"> <button onClick={handleClick}>Manually set your budget goals here</button></div>
   <Balance /> 
   </div>
 
@@ -50,8 +50,10 @@ function mapStateToProps(state) {
     return {
       isFetching: state.plaidReducer.isFetching,
       error: state.plaidReducer.error,
-        linkedAccount: state.loginReducer.user.LinkedAccount
+      linkedAccount: state.loginReducer.user.LinkedAccount,     
+      userId: state.loginReducer.user.id
+
     };
   }
   
-  export default connect(mapStateToProps, { loginUser })(FirstOnboard);
+  export default connect(mapStateToProps, {})(FirstOnboard);

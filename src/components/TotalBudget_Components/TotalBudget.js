e7dbee1c9a540208bee5ea34b7daaf54f52ebc5f
Vanna Sok <53488795+vannasok@users.noreply.github.com>
2020-02-25 14:41:06 -0800

    add progress bar for budgeting

--- src/components/TotalBudget_Components/TotalBudget.js
+++ src/components/TotalBudget_Components/TotalBudget.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import BudgetGoal from './BudgetGoal';
 import TotalExpenses from './TotalExpenses';
+import ProgressBar from './ProgressBar';
+
+import { connect } from 'react-redux';
 
 import './index.css';
 
-const TotalBudget = () => {
+const TotalBudget = props => {
+	const down =
+		Math.round(
+			100 *
+				props.budget.reduce(function(a, b) {
+					return a + b;
+				}, 0)
+		) / 100;
+
+	const up =
+		Math.round(
+			100 *
+				props.expenses.reduce(function(a, b) {
+					return a + b;
+				}, 0)
+		) / 100;
+	const percent = 100;
 	return (
 		<div className='budget-container'>
 			<h5>Budget Summary</h5>
@@ -12,8 +31,21 @@ const TotalBudget = () => {
 				<TotalExpenses />
 				<BudgetGoal />
 			</div>
+			<div className='bar-showcase'>
+				<ProgressBar done={(up / down) * 100} />
+			</div>
 		</div>
 	);
 };
 
-export default TotalBudget;
+function mapStateToProps(state) {
+	return {
+		budget: state.plaidReducer.categories.map(
+			i => Math.round(100 * i.budget) / 100
+		),
+		expenses: state.plaidReducer.categories.map(
+			i => Math.round(100 * i.total) / 100
+		)
+	};
+}
+export default connect(mapStateToProps)(TotalBudget);

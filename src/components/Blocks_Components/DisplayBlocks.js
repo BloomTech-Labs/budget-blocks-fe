257906f83b74cdff9412c46b99bcd109247290f8
Anthony Sutton <anthony.sutton95@gmail.com>
2020-02-26 08:23:05 -0800

    Fixed import error on DisplayBlocks.js

--- src/components/Blocks_Components/DisplayBlocks.js
+++ src/components/Blocks_Components/DisplayBlocks.js
@@ -1,62 +1,58 @@
-import React from 'react';
-import './index.css';
-
+import React from "react";
+import "./index.css";
+import { connect } from "react-redux";
 
 import DeleteBlockModal from "./DeleteBlockModal";
 
-
-const DisplayBlocks = ({ arr, handleClick,LinkedAccount}) => {
-	return (
-		<tbody className='table-body'>
-			{arr.map(i => (
-				<tr key={i.id}>
-					<td>{i.name}</td>
-					<td>
-						$
-						{i.total === null
-							? 0
-							: (Math.round(100 * i.total) / 100).toFixed(2)}
-					</td>
-					<td>
-						$
-						{i.budget === null
-							? 0
-							: (Math.round(100 * i.budget) / 100).toFixed(2)}
-					</td>
-					<td>
-						<DeleteBlockModal blockID={i.id}/>
-						<button
-							id='edit-button'
-							onClick={() => handleClick(i.id, i.budget)}
-						>
-							edit
-						</button>
-					</td>
-			{!LinkedAccount ? <td>
-			<button
-			id='edit-button'
-			onClick={() => handleClick(i.id, i.budget)}
-						>
-							Delete
-						</button>				
-	</td>
-	:
-	null
-	}
-					
-				</tr>
-			))}
-		</tbody>
-	);
+const DisplayBlocks = ({ arr, handleClick, LinkedAccount }) => {
+  return (
+    <tbody className="table-body">
+      {arr.map(i => (
+        <tr key={i.id}>
+          <td>{i.name}</td>
+          <td>
+            $
+            {i.total === null
+              ? 0
+              : (Math.round(100 * i.total) / 100).toFixed(2)}
+          </td>
+          <td>
+            $
+            {i.budget === null
+              ? 0
+              : (Math.round(100 * i.budget) / 100).toFixed(2)}
+          </td>
+          <td>
+            <DeleteBlockModal blockID={i.id} />
+            <button
+              id="edit-button"
+              onClick={() => handleClick(i.id, i.budget)}
+            >
+              edit
+            </button>
+          </td>
+          {!LinkedAccount ? (
+            <td>
+              <button
+                id="edit-button"
+                onClick={() => handleClick(i.id, i.budget)}
+              >
+                Delete
+              </button>
+            </td>
+          ) : null}
+        </tr>
+      ))}
+    </tbody>
+  );
 };
 
 function mapStateToProps(state) {
-	return {
-		userID: state.loginReducer.user.id,
-		LinkedAccount: state.loginReducer.user.LinkedAccount,
-		blocks: state.plaidReducer.categories
-	};
+  return {
+    userID: state.loginReducer.user.id,
+    LinkedAccount: state.loginReducer.user.LinkedAccount,
+    blocks: state.plaidReducer.categories
+  };
 }
 
 export default connect(mapStateToProps)(DisplayBlocks);
-

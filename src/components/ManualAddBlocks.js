bd1facd9731ee9c580642f25c6dddd1104577c58
Lambda_School_Loaner_182 <lambda_school_loaner_182@Lambda-School-Loaner-182s-MacBook-Pro.local>
2020-02-26 10:13:14 -0500

    polished the add blocks modal

--- src/components/ManualAddBlocks.js
+++ src/components/ManualAddBlocks.js
@@ -6,23 +6,57 @@ import { axiosWithAuth } from "./AxiosWithAuth"
 import {Link} from "react-router-dom"
 import { addManualBlocks } from '../redux/actions/ManualActions';
 import "./addBlocks.css"
+import "./Blocks_Components/index.css"
 export const ManualAddBlocks = (props) => {
   
  const [customBlock,setCustomBlock] = useState({name:null,budget:null,total:0})
+ const [failed,setFailed] = useState(false)
  const handleChange = e => {
         setCustomBlock({...customBlock, [e.target.name]: e.target.value});
-
+console.log(customBlock)
     }
     const handleSubmit = e => {
         e.preventDefault()
+        if(customBlock.name !== null && !isNaN(customBlock.budget)){
        props.addManualBlocks(props.userID,customBlock)
-        props.handleClose()
+       setFailed(false)
+       props.handleClose()
+        }
+        else{
+            setFailed(true)
+        }
+       
     }
 
 
   
   return (
     <form onSubmit={handleSubmit}>
+        {
+            failed
+            ?
+            <div>
+            <div className="inputContainer">
+            <h3>Add Custom Manual Blocks</h3>
+            <p className="error-message">Enter a valid name and budget must be a number</p>
+        <input
+        value={customBlock.name}
+        name="name"
+        onChange={handleChange}
+        placeholder="name"
+        />
+         <input
+        value={customBlock.budget}
+        name="budget"
+        onChange={handleChange}
+        placeholder="budget"
+        />
+       
+        </div>
+         <button>Submit</button>
+         </div>
+        :
+        <div>
         <div className="inputContainer">
             <h3>Add Custom Manual Blocks</h3>
         <input
@@ -37,8 +71,12 @@ export const ManualAddBlocks = (props) => {
         onChange={handleChange}
         placeholder="budget"
         />
+       
+        </div>
         <button>Submit</button>
         </div>
+        }
+        
         </form>
   );
 }

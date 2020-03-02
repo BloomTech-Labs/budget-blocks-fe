ac27d08db72f45171b4bea0b3a36a7db140332a1
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-03-02 14:16:01 -0700

    removed commented code in Balance.spec.js

--- src/components/tests/Balance.spec.js
+++ src/components/tests/Balance.spec.js
@@ -1,10 +1,8 @@
 import React from "react";
 import { Balance } from "../Balance_Components/Balance";
-import { render, fireEvent } from "@testing-library/react";
-import { BrowserRouter as Router } from "react-router-dom";
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import { shallow, mount, render as enzymeRender } from "enzyme";
+import { shallow } from "enzyme";
 
 configure({ adapter: new Adapter() });
 
@@ -23,10 +21,3 @@ test("Balance renders Link account component when Linked Account is false", () =
   console.log(wrapper.props().className);
   expect(wrapper.props().className).toBe("makeStyles-NoBalance-1");
 });
-
-// test('Balance renders Balance when Linked Account is true',()=>{
-//     const wrapper = shallow(<Balance LinkedAccount={true} />)
-
-//     const component = wrapper.find("div").at(0);
-//     expect(component.props().className).toBe("Balance");
-// });

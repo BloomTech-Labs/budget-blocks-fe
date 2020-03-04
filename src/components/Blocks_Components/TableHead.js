73ac6ef22c686993df5da73ae334a1b7de46fcc1
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-03-04 15:23:43 -0700

    added comments on TableHead.js

--- src/components/Blocks_Components/TableHead.js
+++ src/components/Blocks_Components/TableHead.js
@@ -3,7 +3,9 @@ import React from 'react';
 import '../../index.css';
 import './index.css';
 
-export const TableHeads = ({ CellNames, className = 'table_names' }) => {
+export const TableHeads = ({ CellNames }) => {
+	// This component creates a row of headers for the LinkedBlocks table
+	// CellNames is an array of all the header names (strings)
 	return (	
 		<thead>
 			<tr className='table-header'>

6ebb7847de59d0e77059bb3384b00258eb71ee5b
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-03-05 15:22:24 -0700

    added comments to Title.js

--- src/components/Form_Components/Title.js
+++ src/components/Form_Components/Title.js
@@ -4,7 +4,7 @@ import logo from "../../media/image/logo.jpg";
 import budgetImg from "../../media/image/budget_blocks.png";
 
 const Title = ({ title }) => {
-
+    // This component displays the Logo and the title of the component (used in login and register)
     return(
         <div className="logo_name">
             <img src={logo} className="logo-reg" alt="logo" />

35dc174589c24472634b4a5f527a4b63bcca565c
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-03-06 11:49:14 -0700

    added comment to Modal_Title.js

--- src/components/Modal_Components/Modal_Title.js
+++ src/components/Modal_Components/Modal_Title.js
@@ -3,7 +3,7 @@ import Typography from "@material-ui/core/Typography";
 import DialogTitle from "./DialogTitle";
 
 export const Modal_Title = ({handleClose, title}) => {
-
+    // This component creates the title for modals
     return(
         <DialogTitle className="customized-dialog-title" onClose={handleClose}>
           <Typography className="customized-dialog-title">

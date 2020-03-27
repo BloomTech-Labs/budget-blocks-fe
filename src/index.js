ed4a22e9215992b1de4cada1255e290c82cf8c5c
debauchery1st <trevor.j.martin@gmail.com>
2020-03-26 22:15:07 -0400

    url central dispatch

--- src/index.js
+++ src/index.js
@@ -9,22 +9,30 @@ import { Provider } from "react-redux";
 import { reducer as loginReducer } from "./redux/reducers/LoginReducer";
 import { reducer as plaidReducer } from "./redux/reducers/PlaidReducer";
 import { reducer as registerReducer } from "./redux/reducers/RegisterReducer";
-import {reducer as blockReducer} from "./redux/reducers/BlockReducers"
-import {reducer as addTransactionReducer} from "./redux/reducers/AddTransactionReducer"
+import { reducer as blockReducer } from "./redux/reducers/BlockReducers";
+import { reducer as addTransactionReducer } from "./redux/reducers/AddTransactionReducer";
 import { BrowserRouter as Router } from "react-router-dom";
-import * as Sentry from '@sentry/browser';
-import dotenv from 'dotenv'
-dotenv.config()
+import * as Sentry from "@sentry/browser";
 
-const store = createStore(combineReducers({loginReducer,plaidReducer,registerReducer,blockReducer,addTransactionReducer}), applyMiddleware(thunk, logger));
-Sentry.init({dsn: "https://d1c8f54ec01c4a3bb2697ec305242356@sentry.io/5172263"});
+const store = createStore(
+  combineReducers({
+    loginReducer,
+    plaidReducer,
+    registerReducer,
+    blockReducer,
+    addTransactionReducer
+  }),
+  applyMiddleware(thunk, logger)
+);
+Sentry.init({
+  dsn: process.env.SENTRY_DSN
+});
 
 ReactDOM.render(
   <Provider store={store}>
-      <Router>
-    <App />
+    <Router>
+      <App />
     </Router>
   </Provider>,
   document.getElementById("root")
 );
-

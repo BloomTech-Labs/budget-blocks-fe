6d24a08b79569a1c4e136d0e74dfc65d2af63bb2
debauchery1st <trevor.j.martin@gmail.com>
2020-03-26 22:59:22 -0400

    environmentUrls from dispatch

--- src/redux/actions/userBlocks.js
+++ src/redux/actions/userBlocks.js
@@ -1,5 +1,6 @@
 import { getManualTrans } from "./ManualActions";
 import { axiosWithAuth } from "../../components/AxiosWithAuth";
+import environmentUrls from "../../dispatch";
 
 export const BLOCKS_CATEGORY_LOADING = "BLOCKS_CATEGORY_LOADING";
 export const BLOCKS_CATEGORY_SUCCESS = "BLOCKS_CATEGORY_SUCCESS";
@@ -13,58 +14,60 @@ export const BLOCKS_DELETE_LOADING = "BLOCKS_CATEGORY_LOADING";
 export const BLOCKS_DELETE_SUCCESS = "BLOCKS_CATEGORY_SUCCESS";
 export const BLOCKS_DELETE_FAILED = "BLOCKS_CATEGORY_FAILED";
 
-
-
 export const blocksLoading = () => ({ type: BLOCKS_CATEGORY_LOADING });
 export const blocksSuccess = data => ({
-    type: BLOCKS_CATEGORY_SUCCESS,
-    payload: data
+  type: BLOCKS_CATEGORY_SUCCESS,
+  payload: data
 });
 export const blocksFailure = error => ({
-    type: BLOCKS_CATEGORY_FAILED,
-    payload: error
+  type: BLOCKS_CATEGORY_FAILED,
+  payload: error
 });
 
 export const updateLoading = () => ({ type: CATEGORY_UPDATE_LOADING });
-export const updateSuccess = data => ({ type: CATEGORY_UPDATE_SUCCESS, payload: data });
+export const updateSuccess = data => ({
+  type: CATEGORY_UPDATE_SUCCESS,
+  payload: data
+});
 export const updateFailed = error => ({
-    type: CATEGORY_UPDATE_FAILED,
-    payload: error
+  type: CATEGORY_UPDATE_FAILED,
+  payload: error
 });
 
-export function updateBlocks(userID, goals){
-    // This function is used to update blocks
-    // Upon success: saves data to redux
-    return function(dispatch) {
-        dispatch(updateLoading());
-        return axiosWithAuth().put(`https://lambda-budget-blocks.herokuapp.com/api/users/categories/${userID}`,goals)
-            .then(response => {
-                dispatch(updateSuccess(response.data));
-            })
-            .catch(error => {
-                dispatch(updateFailed(error));
-            });
-        }
+export function updateBlocks(userID, goals) {
+  // This function is used to update blocks
+  // Upon success: saves data to redux
+  return function(dispatch) {
+    dispatch(updateLoading());
+    return axiosWithAuth()
+      .put(`${environmentUrls.base_url}/api/users/categories/${userID}`, goals)
+      .then(response => {
+        dispatch(updateSuccess(response.data));
+      })
+      .catch(error => {
+        dispatch(updateFailed(error));
+      });
+  };
 }
 
 export function deleteBlocks(userID, blockID) {
-    // This function is used to delete blocks
-    // upon success: calls getManualTrans to retrieve update blocks from back end
-	return function(dispatch) {
-		dispatch({ type: BLOCKS_DELETE_LOADING });
+  // This function is used to delete blocks
+  // upon success: calls getManualTrans to retrieve update blocks from back end
+  return function(dispatch) {
+    dispatch({ type: BLOCKS_DELETE_LOADING });
 
-		return axiosWithAuth()
-			.delete(
-				`https://lambda-budget-blocks.herokuapp.com/manual/categories/${userID}/${blockID}`
-			)
-			.then(response => {
-				return dispatch(getManualTrans(userID));
-			})
-			.catch(error => {
-				dispatch({
-					type: BLOCKS_DELETE_FAILED,
-					payload: error
-				});
-			});
-	};
+    return axiosWithAuth()
+      .delete(
+        `${environmentUrls.base_url}/manual/categories/${userID}/${blockID}`
+      )
+      .then(response => {
+        return dispatch(getManualTrans(userID));
+      })
+      .catch(error => {
+        dispatch({
+          type: BLOCKS_DELETE_FAILED,
+          payload: error
+        });
+      });
+  };
 }

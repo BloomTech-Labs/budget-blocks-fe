c2ce0b1607d353c9e23d0ad741b716a4afdf414c
Trevor Martin <trevor.j.martin@gmail.com>
2020-03-31 16:25:05 -0400

    cleanup state, handlers

--- src/components/Modal_Components/demomanualBlocks.js
+++ src/components/Modal_Components/demomanualBlocks.js
@@ -16,57 +16,64 @@ import "./dialogViews/dialogComponents";
 import "./modalStyle.css";
 
 export const AddTransaction = props => {
-  const [customBlock, setCustomBlock] = useState({
-    name: "",
-    budget: null,
-    total: 0
+  const [state, setState] = useState({
+    customBlock: {
+      name: "",
+      budget: null,
+      total: 0
+    },
+    editBlock: {
+      name: "",
+      budget: null,
+      categoryid: null,
+      total: 0
+    },
+    edit: true,
+    failedEdit: false,
+    failedCustom: false
   });
-  const [editBlock, setEditBlock] = useState({
-    name: "",
-    budget: null,
-    categoryid: null,
-    total: 0
-  });
-  const [failedEdit, setFailedEdit] = useState(false);
-  const [failedCustom, setFailedCustom] = useState(false);
-  const [edit, setEdit] = useState(true);
   const classes = useStyles();
+  const options = [{ label: "Create Block" }];
 
   const handleChange = e => {
-    setCustomBlock({ ...customBlock, [e.target.name]: e.target.value });
-    setEditBlock({ ...editBlock, name: "" });
+    setState({
+      ...state,
+      customBlock: { ...state.customBlock, [e.target.name]: e.target.value },
+      editBlock: { ...state.editBlock, name: "" }
+    });
   };
   const handleEdit = e => {
-    if (cat[e.target.value][1].name === "Create New Category") {
-      setEdit(false);
-    } else {
-      setEdit(true);
-    }
-    setEditBlock({
-      ...editBlock,
-      name: cat[e.target.value][1].name,
-      categoryid: e.target.value,
-      budget: customBlock.budget
+    setState({
+      ...state,
+      editBlock: {
+        ...state.editBlock,
+        name: cat[e.target.value][1].name,
+        categoryid: e.target.value,
+        budget: state.customBlock.budget
+      },
+      customBlock: { ...state.customBlock, name: "" },
+      edit: !(cat[e.target.value][1].name === "Create New Category")
     });
-    setCustomBlock({ ...customBlock, name: "" });
   };
   const handleBudget = e => {
-    setEditBlock({ ...editBlock, budget: e.target.value });
-    setCustomBlock({ ...customBlock, budget: e.target.value });
-    console.log(editBlock);
-    console.log(customBlock);
+    setState({
+      ...state,
+      editBlock: { ...state.editBlock, budget: e.target.value },
+      customBlock: { ...state.customBlock, budget: e.target.value }
+    });
   };
   const handleSubmit = e => {
     e.preventDefault();
-    if (
-      customBlock.name === null ||
-      isNaN(customBlock.budget) ||
-      !isNaN(customBlock.name)
-    ) {
-      setFailedCustom(true);
-    } else {
-      setFailedCustom(false);
-      props.addManualBlocks(props.userID, customBlock);
+    const failing =
+      state.customBlock.name === null ||
+      isNaN(state.customBlock.budget) ||
+      !isNaN(state.customBlock.name);
+    setState({
+      ...state,
+      failedCustom: failing
+    });
+    if (!failing) {
+      props.addManualBlocks(props.userID, state.customBlock);
       props.handleClose();
     }
   };
@@ -74,52 +81,52 @@ export const AddTransaction = props => {
   cat.unshift(["hi", { name: "Create New Category", id: 0 }]);
   const submitEdit = e => {
     e.preventDefault();
-
-    if (editBlock.name === null || isNaN(editBlock.budget)) {
-      setFailedEdit(true);
-    } else {
-      props.editManualBlocks(props.userID, editBlock);
-      setFailedEdit(false);
+    const failing =
+      state.editBlock.name === null || isNaN(state.editBlock.budget);
+    setState({
+      ...state,
+      failedEdit: failing
+    });
+    if (!failing) {
+      props.editManualBlocks(props.userID, state.editBlock);
       props.handleClose();
     }
   };
-  const options = [{ label: "Create Block" }];
   props.blocks
     .filter(i => i.budget === null)
     .map(i => options.push({ value: i.id, label: i.name }));
-
-  console.log(props.blocks);
+  // console.log(props.blocks);
   return (
     <div>
-      {edit && failedEdit
+      {state.edit && state.failedEdit
         ? dialogView1({
             props,
             handleEdit,
             handleBudget,
-            editBlock,
+            editBlock: state.editBlock,
             submitEdit,
             cat,
             classes
           })
-        : edit && !failedEdit
+        : state.edit && !state.failedEdit
         ? dialogView2({
             props,
             handleEdit,
             handleBudget,
-            editBlock,
+            editBlock: state.editBlock,
             submitEdit,
             cat,
             classes
           })
-        : failedCustom && !edit
+        : state.failedCustom && !state.edit
         ? dialogView3({
             props,
             handleEdit,
             handleChange,
             handleBudget,
             handleSubmit,
-            editBlock,
-            customBlock,
+            editBlock: state.editBlock,
+            customBlock: state.customBlock,
             cat,
             classes
           })
@@ -129,8 +136,8 @@ export const AddTransaction = props => {
             handleChange,
             handleBudget,
             handleSubmit,
-            editBlock,
-            customBlock,
+            editBlock: state.editBlock,
+            customBlock: state.customBlock,
             cat,
             classes
           })}

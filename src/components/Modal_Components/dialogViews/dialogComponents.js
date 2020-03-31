427715e0e9726703c0a4cb92005356888ffb296d
Trevor Martin <trevor.j.martin@gmail.com>
2020-03-31 17:42:00 -0400

    described dialogComponents

--- src/components/Modal_Components/dialogViews/dialogComponents.js
+++ src/components/Modal_Components/dialogViews/dialogComponents.js
@@ -5,7 +5,16 @@ import Dialog from "@material-ui/core/Dialog";
 import FormControl from "@material-ui/core/FormControl";
 import TextField from "@material-ui/core/TextField";
 import Select from "@material-ui/core/Select";
-
+/**
+ * @description dialogComponents
+ *
+ * provides one local import containing components common to the dialogViews.
+ *
+ * helps ease the transition from one component framework to another.
+ *
+ * example:
+ *    import {TextField, Button } from "./dialogComponents";
+ */
 export {
   ModalTitle,
   DialogContent,

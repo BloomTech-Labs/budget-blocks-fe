d00a83695114f39490569db40d0afc7e1c5e1d33
Justin Price <mr.price@Justins-iMac.local>
2020-04-14 18:13:24 -0400

    updated LoginActions with promise chain

--- src/redux/actions/LoginActions.js
+++ src/redux/actions/LoginActions.js
@@ -1,35 +1,40 @@
 import axios from "axios";
 import environmentUrls from "../../dispatch";
+import promiseTimeout from "../../helpers/promiseTimeout";
 
 export const LOGIN_USER_LOADING = "LOGIN_USER_LOADING";
 export const LOGIN_USER_SUCCESS = "LOGIN_USER_SUCCESS";
 export const LOGIN_USER_FAILED = "LOGIN_USER_FAILED";
 
 export const loginUserLoading = () => ({ type: LOGIN_USER_LOADING });
-export const loginUserSuccess = data => ({
+export const loginUserSuccess = (data) => ({
   type: LOGIN_USER_SUCCESS,
-  payload: data
+  payload: data,
 });
-export const loginUserFailure = error => ({
+export const loginUserFailure = (error) => ({
   type: LOGIN_USER_FAILED,
-  payload: error
+  payload: error,
 });
 
 export function loginUser(user, history) {
-  return function(dispatch) {
-    dispatch(loginUserLoading());
+  return function (dispatch) {
     return axios
       .post(`${environmentUrls.base_url}/api/auth/login`, user)
-      .then(response => {
+      .then(promiseTimeout(6000))
+      .then((response) => {
+        dispatch(loginUserLoading());
         // All Login data saved to sessionStorage and then saved to redux
         // This way a user can refresh the page a not need to relogin
         sessionStorage.setItem("token", response.data.token);
         sessionStorage.setItem("userID", response.data.id);
         sessionStorage.setItem("LinkedAccount", response.data.LinkedAccount);
         sessionStorage.setItem("navState", response.data.navState);
-        dispatch(loginUserSuccess(response.data));
         localStorage.clear();
+        return response;
+      })
+      .then((response) => {
         // If the user is not using manual nor plaid: take them to onboarding process. Otherwise take them to dashboard
+        dispatch(loginUserSuccess(response.data));
         if (
           response.data.LinkedAccount === true ||
           response.data.ManualOnly === true
@@ -39,9 +44,9 @@ export function loginUser(user, history) {
           history.push("/onBoard/1");
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
-        dispatch(loginUserFailure(error.response.data.message));
+        dispatch(loginUserFailure(error.response.data));
       });
   };
 }

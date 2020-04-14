603b50c36beb2a4eb2b8cea03a83b0244a0aed28
Justin Price <mr.price@Justins-iMac.local>
2020-04-14 18:15:49 -0400

    removed localStorage on register action for security measures

--- src/redux/actions/RegisterActions.js
+++ src/redux/actions/RegisterActions.js
@@ -1,35 +1,36 @@
 import axios from "axios";
 import environmentUrls from "../../dispatch";
+import promiseTimeout from "../../helpers/promiseTimeout";
 
 export const REGISTER_USER_LOADING = "REGISTER_USER_LOADING";
 export const REGISTER_USER_SUCCESS = "REGISTER_USER_SUCCESS";
 export const REGISTER_USER_FAILED = "REGISTER_USER_FAILED";
 
 export const registerUserLoading = () => ({ type: REGISTER_USER_LOADING });
-export const registerUserSuccess = data => ({
+export const registerUserSuccess = (data) => ({
   type: REGISTER_USER_SUCCESS,
-  payload: data
+  payload: data,
 });
-export const registerUserFailure = error => ({
+export const registerUserFailure = (error) => ({
   type: REGISTER_USER_FAILED,
-  payload: error
+  payload: error,
 });
 
 export function registerUser(data, history) {
   // This function is used in Register.jsx
   // After sucecssful register: takes the user to the onboarding process
   // localStorage items are used to login automatically in onboarding process
-  return function(dispatch) {
+
+  return function (dispatch) {
     dispatch(registerUserLoading());
     return axios
       .post(`${environmentUrls.base_url}/api/auth/register`, data)
-      .then(response => {
+      .then(promiseTimeout(6000))
+      .then((response) => {
         dispatch(registerUserSuccess(response.data));
-        localStorage.setItem("email", data.email);
-        localStorage.setItem("password", data.password);
         history.push("/onBoard/1");
       })
-      .catch(error => {
+      .catch((error) => {
         dispatch(registerUserFailure(error.response.data.error));
       });
   };

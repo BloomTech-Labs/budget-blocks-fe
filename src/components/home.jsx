f403707dfa5d5b5e269061b11c9a0743e08e9d77
Justin Price <mr.price@Justins-iMac.local>
2020-04-29 13:06:08 -0400

    assigned the signup button the same class as the sign in button for better styling (signup used to be blue and gave the appearance of being selected when it wasn't) Also changed signup and sign in to register and login

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,46 +1,52 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../media/image/Logo.svg";
 import budgetImg from "../media/image/budget_blocks.png";
 import Container from "@material-ui/core/Container";
 import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
-import {PageView, GAevent} from "./google_analytics/index.js"
-import { Redirect} from "react-router-dom";
+import { PageView, GAevent } from "./google_analytics/index.js";
+import { Redirect } from "react-router-dom";
 
 const Home = () => {
-  useEffect(() =>{
-    PageView()
-  })
+  useEffect(() => {
+    PageView();
+  });
 
-  const [initial, setinitial] = useState(sessionStorage.getItem('token'));
-
-
+  const [initial, setinitial] = useState(sessionStorage.getItem("token"));
 
   let trackSignUp = () => {
-    GAevent("Landing Page Buttons", "User clicked sign-up button", "New User Flow")
-  }
+    GAevent(
+      "Landing Page Buttons",
+      "User clicked sign-up button",
+      "New User Flow"
+    );
+  };
 
-  return ( 
+  return (
     <div className="home">
-    {!initial ? (
-      <Container maxWidth="sm">
-        <div style={{ backgroundColor: "#ffffff", height: "100vh" }}>
-          <img src={logo} className="App-logo" alt="logo" />
-          <img src={budgetImg} className="budget_img" alt="budget_blocks" />
-          <div className="buttons">
-            <Link to="/register" className="links">
-              <Button variant="outlined" className="signup" onClick={trackSignUp}>
-                Sign Up
-              </Button>
-            </Link>
-            <Link to="/login" className="links">
-              <Button variant="outlined" className="signin">
-                Sign In
-              </Button>
-            </Link>
+      {!initial ? (
+        <Container maxWidth="sm">
+          <div style={{ backgroundColor: "#ffffff", height: "100vh" }}>
+            <img src={logo} className="App-logo" alt="logo" />
+            <img src={budgetImg} className="budget_img" alt="budget_blocks" />
+            <div className="buttons">
+              <Link to="/register" className="links">
+                <Button
+                  variant="outlined"
+                  className="signin"
+                  onClick={trackSignUp}
+                >
+                  Register
+                </Button>
+              </Link>
+              <Link to="/login" className="links">
+                <Button variant="outlined" className="signin">
+                  Login
+                </Button>
+              </Link>
+            </div>
           </div>
-        </div>
-      </Container>
+        </Container>
       ) : (
         <Redirect to="/dashboard" />
       )}

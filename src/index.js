70a9b7f5ac14da9e36cd9b2a958718f14e0be39e
William <billy.ryan.rom@gmail.com>
2020-06-05 09:47:58 -0400

    Add Redux Provider to index.js * Add store for global state and apply it to Provider * Add thunk middleware to store * Add rootReducer for combined reducers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
 import App from './App';
 import './index.css';
 
+import rootReducer from './Redux/reducers/index.jsx';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+        <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );

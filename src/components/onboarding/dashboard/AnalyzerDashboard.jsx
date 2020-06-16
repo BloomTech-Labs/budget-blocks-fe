02bac80fc32449dd73e40c41372e9501e9429f7a
Aleksei-Zaichenko <aleksei.zaichenko@cix.csi.cuny.edu>
2020-06-16 18:25:57 -0400

    Adds styling and functionality for page components in file AnalyzerDashboard.jsx

--- src/components/onboarding/dashboard/AnalyzerDashboard.jsx
+++ src/components/onboarding/dashboard/AnalyzerDashboard.jsx
@@ -1,23 +1,20 @@
 import React, { useState } from 'react';
-import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import Avatar from '@material-ui/core/Avatar';
 import {
-    TextField,
     Button,
     makeStyles,
-    withStyles,
-    Select,
-    MenuItem,
+    Grid,
+    Divider,
+    Box
 } from '@material-ui/core';
+import ProgressBar from 'react-bootstrap/ProgressBar'
 import { connect } from 'react-redux';
-import Grid from '@material-ui/core/Grid';
 
 const message = `Truncation should be conditionally applicable on this long line of text
  as this is a much longer line than what the container can support. `;
 
 const useStyles = makeStyles((theme) => ({
-    container: {
+    root: {
         display: "flex",
         flexDirection: "column",
         wrap: "nowrap",
@@ -26,6 +23,9 @@ const useStyles = makeStyles((theme) => ({
         textAlign: "center",
         backgroundColor: 'rgba(19, 185, 172, 0.7)',
         color: 'white',
+    },
+    lowerContainer: {
+        margin: "0 3%",
     },
     componentButton: {
         fontSize: '1.4rem',
@@ -38,9 +38,6 @@ const useStyles = makeStyles((theme) => ({
             textDecoration: 'underline',
         },
     },
-    buttonsClass: {
-        justifyContent: "center",
-    },
     hiMessage: {
         [theme.breakpoints.up('md')]: {
             marginTop: "1%"
@@ -51,28 +48,116 @@ const useStyles = makeStyles((theme) => ({
     },
     balance: {
         marginBottom: "4%",
+    },
+    blocksButtonFalse: {
+        backgroundColor: "#F6F6F6"
+    },
+    blocksButtonTrue: {
+        color: 'white',
+        backgroundColor: '#5ACEC5'
+    },
+    thirdContainer: {
+        margin: "3% 0 2% 0",
+    },
+    monthsButtonsBoxClass: {
+        border: "1px solid black",
+    },
+    blocksButtonsBoxClass: {
+        width: "54.5%",
+        display: 'flex',
+        justifyContent: 'space-between',
     }
 }));
 
 const AnalyzerDashboard = (props) => {
 
     const classes = useStyles();
+    const [activeComponent, setActiveComponent] = useState('budget');
+
+    const [incomeButtonClass, setIncomeButtonClass] = useState(false);
+    const [housingButtonClass, setHousingButtonClass] = useState(false);
+    const [foodBuButtonClass, setFoodButtonClass] = useState(false);
+    const [carBuButtonClass, setCarButtonClass] = useState(false);
+    const [personalBuButtonClass, setPersonalButtonClass] = useState(false);
+
+    const handleSpending = () => {
+        setActiveComponent('spending')
+    };
+
+    const handleBudget = () => {
+        setActiveComponent('budget')
+    };
+
+    const setButtonsClasses = (IncomeButtonOn = false, HousingButtonOn = false, FoodButtonOn = false, CarButtonOn = false, PersonalButtonOn = false) => {
+        setIncomeButtonClass(IncomeButtonOn);
+        setHousingButtonClass(HousingButtonOn);
+        setFoodButtonClass(FoodButtonOn);
+        setCarButtonClass(CarButtonOn);
+        setPersonalButtonClass(PersonalButtonOn);
+    }
+
+    const handleIncomeClick = (e) => {
+        setButtonsClasses(true);
+    }
+
+    const handleHousing = (e) => {
+        setButtonsClasses(false, true);
+    }
+
+    const handleFood = (e) => {
+        setButtonsClasses(false, false, true);
+    }
+
+    const handleCar = (e) => {
+        setButtonsClasses(false, false, false, true);
+    }
+
+    const handlePersonal = (e) => {
+        setButtonsClasses(false, false, false, false, true);
+    }
 
     return (
         <div className={classes.root}>
-            <Grid container className={classes.container}>
-                <Grid item xs={12}>
-                    <Grid container className={classes.topContainer}>
-                        <Grid item xs={12} className={classes.hiMessage}><Typography variant="h5">Hi {props.userInfo.name}!</Typography></Grid>
-                        <Grid item xs={12}><Typography variant="h6">Your balance is</Typography></Grid>
-                        <Grid item xs={12} className={classes.balance}><Typography variant="h3">${}</Typography></Grid>
-                        <Grid item xs={12} classname={classes.buttonsClass}>
-                            <Button className={classes.componentButton} > Spending </Button>
-                            <Button className={classes.componentButton} > Budget </Button>
-                        </Grid>
-                    </Grid>
+            <Grid container className={classes.topContainer}>
+                <Grid item xs={12} className={classes.hiMessage}>Hi {props.userInfo.name}!</Grid>
+                <Grid item xs={12}><Typography variant="h6">Your balance is</Typography></Grid>
+                <Grid item xs={12} className={classes.balance}><Typography variant="h3">${}</Typography></Grid>
+                <Grid item xs={12} classname={classes.buttonsClass}>
+                    <Button className={classes.componentButton} onClick={handleSpending}> Spending </Button>
+                    <Button className={classes.componentButton} onClick={handleBudget}> Budget </Button>
                 </Grid>
             </Grid>
+            <div>
+                {activeComponent === 'budget' ? (
+                    <Grid container className={classes.lowerContainer}>
+                        <Grid item xs={12}>
+                            <Box className={classes.monthsButtonsBoxClass}>
+                                <Button style={{ border: "1px solid black", textAlign: "left" }}>May</Button>
+                                <Button>Jun</Button>
+                                <Button>Jul</Button>
+                                <Button>Aug</Button>
+                                <Button>Sep</Button>
+                                <Button>Oct</Button>
+                                <Button>Nov</Button>
+                                <Button>Dec</Button>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={12} className={classes.thirdContainer}>
+                            <Typography >Blocks</Typography>
+                            <Box className={classes.blocksButtonsBoxClass}>
+                                <Button className={incomeButtonClass ? classes.blocksButtonTrue : classes.blocksButtonFalse} onClick={handleIncomeClick}>Income</Button>
+                                <Button className={housingButtonClass ? classes.blocksButtonTrue : classes.blocksButtonFalse} onClick={e => handleHousing(e)}>House</Button>
+                                <Button className={foodBuButtonClass ? classes.blocksButtonTrue : classes.blocksButtonFalse} onClick={e => handleFood(e)}>Food</Button>
+                                <Button className={carBuButtonClass ? classes.blocksButtonTrue : classes.blocksButtonFalse} onClick={e => handleCar(e)}>Car</Button>
+                                <Button className={personalBuButtonClass ? classes.blocksButtonTrue : classes.blocksButtonFalse} onClick={e => handlePersonal(e)}>Personal</Button>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Typography >Income ${}</Typography>
+                        </Grid>
+                    </Grid>
+                ) : (<h1>Spending button was clicked</h1>)}
+            </div>
         </div>
     );
 };
